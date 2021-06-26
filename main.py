a826dd1045e1e2ea9fa3fe17992fe7346c578861
hamedpro <hamedyaghootpour@gmail.com>
2021-06-27 00:30:27 +0430

    some works

--- main.py
+++ main.py
@@ -5,7 +5,48 @@ def countEachItem(array):
             results[array[i]] += 1
         else:
             results[array[i]] = 1
-    return results    
+    return results
+class ring:
+    duration = 90
+    def analyse(self):
+        pass
+class day:
+    rings = [] # ring objects 
+    ringNames = [] # name prop of ring objects 
+    dayNumber = None
+    def analyse(self):
+        for ring in self.rings:
+            self.ringNames.append(ring.name)
+        for i in range(len(self.rings)):
+            self.rings[i].analyse()
+        self.lessonsCount = common.countEachItem(self.ringNames)
+        self.dayName = myConvertor.dayNumberToDayName(self.dayNumber)
+        
+class classRoom:
+    days = []
+    def analyse(self):
+        for i in range(len(self.days)):
+            self.days[i].analyse()
+class school:
+    classRooms = []
+    def analyse(self):
+        for i in range(len(self.classRooms)):
+            self.classRooms[i].analyse()
+
+
+
+def buildSchoolFromArray(array,RingsCount,daysCount,classRoomsCount):
+    tmpDay = day()
+    for i in range(RingsCount):
+        tmpDay.rings.append(ring())
+    tmpClassRoom = classRoom()
+    for i in range(daysCount):
+        classRoom.days.append(day())
+    tmpSch = school()
+    for i in range(classRoomsCount):
+        tmpSch.classRooms.append(classRoom())
+    return tmpSch
+
 def printWithIndent(string,indentCount):
     modifiedString = ""
     for i in range(indentCount):
@@ -19,12 +60,12 @@ def printSchool(school):
     except:
         print("[school name]")
         
-    for week in school.weeks:
+    for classRoom in school.classRooms:
         try:
-            printWithIndent(week.name,1)
+            printWithIndent(classRoom.name,1)
         except:
             printWithIndent("[week name]",1)
-        for day in week.days:
+        for day in classRoom.days:
             try:
                 printWithIndent(day.name,2)
             except:
@@ -34,7 +75,7 @@ def printSchool(school):
                     printWithIndent(ring.name,3)
                 except:
                     printWithIndent("[ring name]",3)
-                    
+
 
 lessons = ["farsi","english","lesson 3"]
 results = []
@@ -52,8 +93,7 @@ def fill_next_cell(array):
             fill_next_cell(cloned)
 
 fill_next_cell([])
-for i in results:
-    print(i)
+
 
 
 
